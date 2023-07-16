9cbc500ff8714a90b2d839f0625ed14fcb697ff6
Nameless <85962933+Misfiy@users.noreply.github.com>
2023-07-16 14:39:00 +0200

    fix

--- Events/PlayerHandler.cs
+++ Events/PlayerHandler.cs
@@ -1,22 +1,28 @@
 namespace BleedingPlugin.Events
 {
-     using Exiled.Events.EventArgs.Player;
-     using Exiled.API.Enums;
-     using PlayerRoles;
-     using BleedingPlugin;
-     using Exiled.API.Features;
-     public sealed class PlayerHandler
-     {
-          Config config = Bleeding.Instance.Config;
-          public void OnHurting(HurtingEventArgs ev)
-          {
-               if(!config.CanScpsBleed && ev.Attacker != null && ev.Attacker.IsScp) return;
-               if(config.DamageTypes.Contains(ev.DamageHandler.Type))
-               if(ev.Attacker != null && config.RoleTypes.Contains(ev.Attacker.Role.Type)) { ev.Player.EnableEffect(EffectType.Bleeding); }
-          }
-          public void OnUsedItem(UsedItemEventArgs ev)
-          {
-               if(config.CureItems.Contains(ev.Item.Type)) ev.Player.DisableEffect(EffectType.Bleeding);
-          }
-     }
+    using Exiled.Events.EventArgs.Player;
+    using Exiled.API.Enums;
+    using PlayerRoles;
+    using BleedingPlugin;
+    using Exiled.API.Features;
+    public sealed class PlayerHandler
+    {
+        Config config = Bleeding.Instance.Config;
+        public void OnHurting(HurtingEventArgs ev)
+        {
+            string attackerName = ev.Attacker != null ? ev.Attacker.ToString() : "No attacker";
+            Log.Debug($"{ev.Player.Nickname} was hurt: {ev.DamageHandler.Type}. Attacker: {attackerName}");
+            if (!config.CanScpsBleed && ev.Attacker != null && ev.Attacker.IsScp) return;
+            if (config.DamageTypes.Contains(ev.DamageHandler.Type)) ev.Player.EnableEffect(EffectType.Bleeding);
+            if (ev.Attacker != null && config.RoleTypes.Contains(ev.Attacker.Role.Type)) ev.Player.EnableEffect(EffectType.Bleeding);
+        }
+        public void OnUsedItem(UsedItemEventArgs ev)
+        {
+            if (config.CureItems.Contains(ev.Item.Type)) ev.Player.DisableEffect(EffectType.Bleeding);
+        }
+        private void EnableBleed(Player player)
+        {
+            player.EnableEffect(EffectType.Bleeding);
+        }
+    }
 }
